008938baa150f58b0d58860078d8f9c1bcfe77b2
danmaclean <maclean.daniel@gmail.com>
2015-12-04 17:31:14 +0000

    camera method

--- opimage_things/cameras.py
+++ opimage_things/cameras.py
@@ -2,8 +2,13 @@
 
 import picamera
 
-def take_picture(fname='pic.jpg', hflip=False, vflip=True)
-    cam = picamera.PiCamera()
-    cam.hflip=hflip
-    cam.vflip=vflip
-    cam.capture(fname)
+def take_picture(fname='pic.jpg', hflip=False, vflip=True, width=600, height=480, fmt="jpeg",shutter_speed=100000):
+    with picamera.PiCamera() as cam:
+        cam.hflip=hflip
+        cam.vflip=vflip
+        cam.resolution = (width,height)
+        cam.shutter_speed = shutter_speed
+        if fmt == 'rgb':
+	    cam.capture(fname, 'rgb')
+        else:
+    	    cam.capture(fname, fmt)
